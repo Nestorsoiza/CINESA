db04434cd9be8f45534b29ae02287ee5c9d34054
Nestorsoiza <nestor.soiza83@gmail.com>
2020-12-02 21:09:15 +0100

    Realizo clase asiento
    el public boolean ocupado() {
    		return espectador != null;
    	}
    le quiero revisar, ¿Interfaces?

--- src/cinema/Asiento.java
+++ src/cinema/Asiento.java
@@ -1,5 +1,53 @@
 package cinema;
 
 public class Asiento {
+	private char letra;
+	private int fila;
+	private Espectador espectador;
+	
+	public Asiento(char letra, int fila) {
+		this.letra = letra;
+		this.fila = fila;
+		this.espectador = null;
+	}
 
+	public char getLetra() {
+		return letra;
+	}
+
+	public void setLetra(char letra) {
+		this.letra = letra;
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int fila) {
+		this.fila = fila;
+	}
+
+	public Espectador getEspectador() {
+		return espectador;
+	}
+
+	public void setEspectador(Espectador espectador) {
+		this.espectador = espectador;
+	}
+	
+	public boolean ocupado() {
+		return espectador != null;
+	}
+
+	@Override
+	public String toString() {
+		if (ocupado()) {
+			return "Asiento: "  + letra + fila + " y " + espectador;
+	
+		}
+		return "Asiento: "  + letra + fila + " y este asiento está vacío";
+	}
+	 
+	
+	
 }
